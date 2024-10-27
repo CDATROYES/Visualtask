b63fca8a6b1e6ace8d6138180b8f905c573f12aa
CDATROYES <backoffice.troyes@gmail.com>
2024-10-27 08:49:24 +0100

    Create CSVViewer.tsx

---
+++ components/client/CSVViewer.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import React, { useState, useEffect, useCallback } from 'react';
+import { parse, unparse } from 'papaparse';
+import { Edit2, Save, X } from 'lucide-react';
+
+// Copiez ici tout le contenu de votre ancien CSVViewer.tsx
