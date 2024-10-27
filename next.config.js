6fcdb57e1ffc252bdde2a07684f63aff83dbd263
CDATROYES <backoffice.troyes@gmail.com>
2024-10-27 10:40:57 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    CI: 'false'
+  },
+  // Ajoutez ces lignes
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
