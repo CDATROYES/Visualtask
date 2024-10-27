5c58eed4cbe8a92e1f21469193e126f2544bf18f
CDATROYES <backoffice.troyes@gmail.com>
2024-10-27 10:36:42 +0100

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -8,7 +8,7 @@ const Card = React.forwardRef
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={`rounded-lg border bg-card text-card-foreground shadow-sm ${className}`}
+    className={`rounded-lg border bg-card text-card-foreground shadow-sm ${className || ''}`}
     {...props}
   />
 ))
@@ -20,7 +20,7 @@ const CardHeader = React.forwardRef
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={`flex flex-col space-y-1.5 p-6 ${className}`}
+    className={`flex flex-col space-y-1.5 p-6 ${className || ''}`}
     {...props}
   />
 ))
@@ -32,7 +32,7 @@ const CardTitle = React.forwardRef
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={`text-2xl font-semibold leading-none tracking-tight ${className}`}
+    className={`text-2xl font-semibold leading-none tracking-tight ${className || ''}`}
     {...props}
   />
 ))
@@ -42,7 +42,11 @@ const CardContent = React.forwardRef
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={`p-6 pt-0 ${className}`} {...props} />
+  <div 
+    ref={ref} 
+    className={`p-6 pt-0 ${className || ''}`}
+    {...props}
+  />
 ))
 CardContent.displayName = "CardContent"
 
