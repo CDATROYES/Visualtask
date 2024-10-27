f1d07eda0833b38ff56f5f32d3da839ef5d8f83c
CDATROYES <backoffice.troyes@gmail.com>
2024-10-27 09:44:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import { Suspense } from 'react';
 import dynamic from 'next/dynamic'
 
 const CSVViewer = dynamic(
-  async () => {
-    const module = await import('@/components/client/CSVViewer');
-    return module.default;
-  },
+  () => import('@/components/client/CSVViewer'),
   {
     ssr: false,
     loading: () => <div className="p-4">Chargement...</div>
@@ -17,9 +13,7 @@ const CSVViewer = dynamic(
 export default function Page() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Suspense fallback={<div className="p-4">Chargement...</div>}>
-        <CSVViewer />
-      </Suspense>
+      <CSVViewer />
     </div>
   )
 }
