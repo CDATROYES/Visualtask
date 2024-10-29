3815256d12f4eff1bd8ce47780bd7fbe845da74a
CDATROYES <backoffice.troyes@gmail.com>
2024-10-29 17:37:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,39 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    CI: 'false'
+    CI: 'false',
+    NEXT_TELEMETRY_DISABLED: '1'
   },
-  // Ajoutez ces lignes
+  experimental: {
+    optimizeFonts: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
     };
+
+    // Ajout de la configuration pour gérer les modules problématiques
+    config.module = {
+      ...config.module,
+      exprContextCritical: false,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.m?js$/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+      ],
+    };
+
     return config;
+  },
+  transpilePackages: ['lucide-react', 'papaparse'],
+  // Optimisations pour la production
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
   },
 }
 
